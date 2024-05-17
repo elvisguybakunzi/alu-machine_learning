adb54a66dbd2f82e8f344746c07e3731edbf455f
elvisguybakunzi <e.bakunzi@alustudent.com>
2024-05-17 18:28:21 +0200

    adding a documentation

--- math/linear_algebra/2-size_me_please.py
+++ math/linear_algebra/2-size_me_please.py
@@ -1,4 +1,21 @@
+#!/usr/bin/env python3
+"""
+This Python script is used to calculate shape of matrix.
+
+"""
+
 def matrix_shape(matrix):
+
+    """
+    Calculates the shape of a matrix.
+
+    Parameters:
+    matrix (list): A list of lists (or nested lists) representing the matrix.
+
+    Returns:
+    list: A list of integers representing the dimensions of the matrix.
+
+    """
     if not isinstance(matrix, list):
         return []
     return [len(matrix)] + matrix_shape(matrix[0])
