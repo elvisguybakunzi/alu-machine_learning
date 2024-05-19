25b21fe8a143e460d3d8bf492da8dc5e83cfed3c
elvisguybakunzi <e.bakunzi@alustudent.com>
2024-05-19 20:42:11 +0200

    script that calculates the shape of matrix

--- math/linear_algebra/10-ill_use_my_scale.py
+++ math/linear_algebra/10-ill_use_my_scale.py
@@ -10,5 +10,9 @@ import numpy as np
 
 
 def np_shape(matrix):
-    """Calculate the shape of a numpy.ndarray."""
-    return matrix.shape
+    """Calculate the shape of a nested list."""
+    shape = []
+    while isinstance(matrix, list):
+        shape.append(len(matrix))
+        matrix = matrix[0] if matrix else None
+    return tuple(shape)
