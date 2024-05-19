34f7652b3f2600aaeeff40e312cfebbd6785b40e
elvisguybakunzi <e.bakunzi@alustudent.com>
2024-05-19 21:26:12 +0200

    correction not importing any module

--- math/linear_algebra/10-ill_use_my_scale.py
+++ math/linear_algebra/10-ill_use_my_scale.py
@@ -1,18 +1,12 @@
 #!/usr/bin/env python3
 
-"""
-This script calculates the shape of numpy.ndarray.
-
-"""
-
 
 def np_shape(matrix):
-    """Calculate the shape of a numpy.ndarray represented as nested lists."""
-    shape = []
-    while isinstance(matrix, list):
-        shape.append(len(matrix))
-        if len(matrix) > 0:
-            matrix = matrix[0]
-        else:
-            break
-    return tuple(shape)
+    """Calculate the shape of a nested list simulating a numpy.ndarray."""
+    def shape_rec(mat, shape=[]):
+        shape.append(len(mat))
+        if len(mat) > 0 and isinstance(mat[0], list):
+            return shape_rec(mat[0], shape)
+        return shape
+    
+    return tuple(shape_rec(matrix))
