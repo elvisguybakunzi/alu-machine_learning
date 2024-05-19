4d2db3cbbb08e870cbbf1e8db3c7598fde8c58d6
elvisguybakunzi <e.bakunzi@alustudent.com>
2024-05-19 20:38:27 +0200

    script that calculates the shape of matrix

---
+++ math/linear_algebra/10-ill_use_my_scale.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python3
+
+"""
+This script calculates the shape of numpy.ndarray.
+
+"""
+
+
+import numpy as np
+
+
+def np_shape(matrix):
+    """Calculate the shape of a numpy.ndarray."""
+    return matrix.shape
