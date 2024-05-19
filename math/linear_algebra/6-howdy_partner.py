41f55af95e1769ad8cbba998b39a10e8dfb6969a
elvisguybakunzi <e.bakunzi@alustudent.com>
2024-05-19 17:20:39 +0200

    script that concatenate two arrays

---
+++ math/linear_algebra/6-howdy_partner.py
@@ -0,0 +1,22 @@
+#!/usr/bin/env python3
+
+"""
+THis Script concatenates two arrays.
+
+"""
+
+
+def cat_arrays(arr1, arr2):
+    """
+    Concatenates two arrays.
+
+    Parameters:
+    arr1 (list of ints/floats): The first array.
+    arr2 (list of ints/floats): The second array.
+
+    Returns:
+    list of ints/floats: A new list containing the
+    elements of arr1 followed by the elements of arr2.
+
+    """
+    return arr1 + arr2
