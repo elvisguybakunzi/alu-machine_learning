a54bbb397e52672a1111fbab2216b06713a9360c
elvisguybakunzi <e.bakunzi@alustudent.com>
2024-06-09 07:49:48 +0200

    exponential script

--- math/probability/exponential.py
+++ math/probability/exponential.py
@@ -1,64 +1,22 @@
 #!/usr/bin/env python3
-
-'''class Poisson that sets the lambtha
-of a poisson distribution
-Args = data
-'''
+"""
+Exponential module
+"""
 
 
 class Exponential:
-    '''class constructor'''
+    """Represents an Exponential distribution"""
 
     def __init__(self, data=None, lambtha=1.):
-        try:
-            if data is None:
-                if lambtha < 1:
-                    raise ValueError("lambtha must be a positive value")
-                else:
-                    self.lambtha = float(lambtha)
-            else:
-                if type(data) is not list:
-                    raise TypeError("data must be a list")
-                elif len(data) < 2:
-                    raise ValueError("data must contain multiple values")
-                else:
-                    lambtha = float(len(data) / sum(data))
-                    self.lambtha = lambtha
-        except Exception as e:
-            print(str(e))
-
-    def pdf(self, x):
-        """
-        calculates the value of the PDF for a given time period
-
-        parameters:
-            x [int]: time period
-                If x is out of range, return 0
-
-        return:
-            the PDF value for x
-        """
-        if x < 0:
-            return 0
-        e = 2.7182818285
-        lambtha = self.lambtha
-        pdf = lambtha * (e ** (-lambtha * x))
-        return pdf
-
-    def cdf(self, x):
-        """
-        calculates the value of the CDF for a given time period
-
-        parameters:
-            x [int]: time period
-                If x is out of range, return 0
-
-        return:
-            the CDF value for x
-        """
-        if x < 0:
-            return 0
-        e = 2.7182818285
-        lambtha = self.lambtha
-        cdf = 1 - (e ** (-lambtha * x))
-        return cdf
+        """Initialize Exponential distribution"""
+        if data is None:
+            if lambtha <= 0:
+                raise ValueError("lambtha must be a positive value")
+            self.lambtha = float(lambtha)
+        else:
+            if not isinstance(data, list):
+                raise TypeError("data must be a list")
+            if len(data) < 2:
+                raise ValueError("data must contain multiple values")
+            # Calculate lambtha as the inverse of the mean of the data
+            self.lambtha = float(1 / (sum(data) / len(data)))
