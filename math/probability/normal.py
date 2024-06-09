f7e230ff021455133d895be3540e5292cfdc4210
elvisguybakunzi <e.bakunzi@alustudent.com>
2024-06-09 08:21:02 +0200

    normal script

---
+++ math/probability/normal.py
@@ -0,0 +1,25 @@
+#!/usr/bin/env python3
+"""
+Normal module
+"""
+
+
+class Normal:
+    """Represents a Normal distribution"""
+
+    def __init__(self, data=None, mean=0., stddev=1.):
+        """Initialize Normal distribution"""
+        if data is None:
+            if stddev <= 0:
+                raise ValueError("stddev must be a positive value")
+            self.mean = float(mean)
+            self.stddev = float(stddev)
+        else:
+            if not isinstance(data, list):
+                raise TypeError("data must be a list")
+            if len(data) < 2:
+                raise ValueError("data must contain multiple values")
+            # Calculate mean and standard deviation from data
+            self.mean = sum(data) / len(data)
+            variance = sum((x - self.mean) ** 2 for x in data) / len(data)
+            self.stddev = variance ** 0.5
