9768701328e901c8d742e97d92d9606ae57392e0
elvisguybakunzi <e.bakunzi@alustudent.com>
2024-06-11 19:14:17 +0200

    With updated MultiNormal class it is used to find pdf

--- math/multivariate_prob/multinormal.py
+++ math/multivariate_prob/multinormal.py
@@ -47,3 +47,27 @@ class MultiNormal:
     @cov.setter
     def cov(self, value):
         self._cov = value
+
+    def pdf(self, x):
+        """
+        Calculates the PDF at a data point.
+        Parameters:
+        x (numpy.ndarray): The data point of shape (d, 1)
+        Raises:
+        TypeError: If x is not a numpy.ndarray
+        ValueError: If x is not of shape (d, 1)
+        Returns:
+        float: The value of the PDF
+        """
+        if not isinstance(x, np.ndarray):
+            raise TypeError("x must be a numpy.ndarray")
+        d = self.mean.shape[0]
+        if x.shape != (d, 1):
+            raise ValueError("x must have the shape ({}, 1)".format(d))
+        mean_diff = x - self.mean
+        cov_inv = np.linalg.inv(self.cov)
+        det_cov = np.linalg.det(self.cov)
+        numerator = np.exp(-0.5 * (mean_diff.T @ cov_inv @ mean_diff))
+        denominator = np.sqrt((2 * np.pi) ** d * det_cov)
+
+        return float(numerator / denominator)
