74479ab32ece68bc4247421fb799e884bf7d430d
elvisguybakunzi <e.bakunzi@alustudent.com>
2024-09-12 21:43:38 +0200

    added the first program and README

---
+++ classification/0-neuron.py
@@ -0,0 +1,29 @@
+#!/user/bin/env python3
+import numpy as np
+
+
+class Neuron:
+    """Defines a single neuron performing binary classification"""
+    def __init__(self, nx):
+        """
+        Initializes a neuron for binary classification.
+
+        Args:
+            nx (integer): The number odf input features to the neuron.
+        Raises:
+            TypeError: If nx is not an integer.
+            ValueError: If nx is less than 1.
+        """
+        if not isinstance(nx, int):
+            raise TypeError("nx must be an integer")
+        if nx < 1:
+            raise ValueError("nx must be a positive integer")
+
+        # initialize weights using random normal distribution
+        self.W = np.random.randn(1, nx)
+
+        # initialize bias to 0
+        self.b = 0
+
+        # initialize activated output (predicted) to 0
+        self.A = 0
