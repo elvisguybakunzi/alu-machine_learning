a5c176afbb6dcbc23469ff2c7429bd18147949f4
elvisguybakunzi <e.bakunzi@alustudent.com>
2024-09-13 15:11:25 +0200

    updated class NeuralNetwork

--- supervised_learning/classification/9-neural_network.py
+++ supervised_learning/classification/9-neural_network.py
@@ -40,12 +40,43 @@ class NeuralNetwork:
         if nodes < 1:
             raise ValueError("nodes must be a positive integer")
 
-        # Hidden layers weights, biases, and activation
-        self.W1 = np.random.randn(nodes, nx)
-        self.b1 = np.zeros((nodes, 1))
-        self.A1 = 0
+        # Private attributes Hidden layers
+        self.__W1 = np.random.randn(nodes, nx)
+        self.__b1 = np.zeros((nodes, 1))
+        self.__A1 = 0
 
-        # Output layer weights, biases, and activation
-        self.W2 = np.random.randn(1, nodes)
-        self.b2 = np.zeros((1, 1))
-        self.A2 = 0
+        # Private attributes Output layer
+        self.__W2 = np.random.randn(1, nodes)
+        self.__b2 = np.zeros((1, 1))
+        self.__A2 = 0
+
+    # Getter methods for each private attributes
+    @property
+    def W1(self):
+        """Getter for W1"""
+        return self.__W1
+
+    @property
+    def b1(self):
+        """Getter for b1"""
+        return self.__b1
+
+    @property
+    def A1(self):
+        """Getter for A1"""
+        return self.__A1
+
+    @property
+    def W2(self):
+        """Getter for W2"""
+        return self.__W2
+
+    @property
+    def b2(self):
+        """Getter for b2"""
+        return self.__b2
+
+    @property
+    def A2(self):
+        """Getter for A2"""
+        return self.__A2
