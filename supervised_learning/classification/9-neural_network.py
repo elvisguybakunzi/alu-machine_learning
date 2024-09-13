ad5657897652d9fd2d83118d6f24196491bee239
elvisguybakunzi <e.bakunzi@alustudent.com>
2024-09-13 19:32:58 +0200

    updated class NeuralNetwork

--- supervised_learning/classification/9-neural_network.py
+++ supervised_learning/classification/9-neural_network.py
@@ -47,7 +47,7 @@ class NeuralNetwork:
 
         # Private attributes Output layer
         self.__W2 = np.random.randn(1, nodes)
-        self.__b2 = np.zeros((1, 1))
+        self.__b2 = 0
         self.__A2 = 0
 
     # Getter methods of each private attributes
