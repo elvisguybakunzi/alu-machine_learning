59cd22bacf1d8b65c150c996f151b37a575f10b5
elvisguybakunzi <e.bakunzi@alustudent.com>
2024-09-28 13:28:44 +0200

    gradient descent

--- supervised_learning/regularization/1-l2_reg_gradient_descent.py
+++ supervised_learning/regularization/1-l2_reg_gradient_descent.py
@@ -23,26 +23,27 @@ def l2_reg_gradient_descent(Y, weights, cache, alpha, lambtha, L):
 
     The weights and biases are updated in place.
     """
-    m = Y.shape[1]  # Number of data points
-    A_last = cache['A' + str(L)]  # Output of the last layer
-
-    # Derivative of cost with respect to Z for the output layer
-    dZ = A_last - Y
-
-    for l in range(L, 0, -1):
-        A_prev = cache['A' + str(l - 1)]  # Output of the previous layer
-        W = weights['W' + str(l)]  # Weights of the current layer
-        b = weights['b' + str(l)]  # Biases of the current layer
-
-        # Gradient of weights and biases with L2 regularization
-        dW = (1 / m) * np.matmul(dZ, A_prev.T) + (lambtha / m) * W
+    m = Y.shape[1]
+    
+    # Compute the gradient for the output layer
+    dZ = cache['A{}'.format(L)] - Y
+    dW = (1 / m) * np.dot(dZ, cache['A{}'.format(L-1)].T) + (lambtha / m) * weights['W{}'.format(L)]
+    db = (1 / m) * np.sum(dZ, axis=1, keepdims=True)
+    
+    # Update weights and biases for the output layer
+    weights['W{}'.format(L)] -= alpha * dW
+    weights['b{}'.format(L)] -= alpha * db
+    
+    # Compute gradients and update weights for hidden layers
+    for l in range(L-1, 0, -1):
+        dA = np.dot(weights['W{}'.format(l+1)].T, dZ)
+        dZ = dA * (1 - np.power(cache['A{}'.format(l)], 2))
+        dW = (1 / m) * np.dot(dZ, cache['A{}'.format(l-1)].T) + (lambtha / m) * weights['W{}'.format(l)]
         db = (1 / m) * np.sum(dZ, axis=1, keepdims=True)
-
+        
         # Update weights and biases
-        weights['W' + str(l)] -= alpha * dW
-        weights['b' + str(l)] -= alpha * db
-
-        if l > 1:
-            # For layers before the last one, calculate dZ for the iteration
-            dA_prev = np.matmul(W.T, dZ)
-            dZ = dA_prev * (1 - A_prev ** 2)  # Derivative of tanh activation
+        weights['W{}'.format(l)] -= alpha * dW
+        weights['b{}'.format(l)] -= alpha * db
+        
+        # Update dZ for next iteration
+        dZ = dZ
