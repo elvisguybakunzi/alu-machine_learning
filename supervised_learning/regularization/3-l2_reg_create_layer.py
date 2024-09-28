962db19e61fab3ea0c3d8c415c2a8b7bd8e11ad3
elvisguybakunzi <e.bakunzi@alustudent.com>
2024-09-28 13:22:23 +0200

    create layer

--- supervised_learning/regularization/3-l2_reg_create_layer.py
+++ supervised_learning/regularization/3-l2_reg_create_layer.py
@@ -1,28 +1,32 @@
 #!/usr/bin/env python3
+"""Create layer"""
+
 import tensorflow as tf
+
 
 def l2_reg_create_layer(prev, n, activation, lambtha):
     """
     Creates a tensorflow layer that includes L2 regularization.
-    
+
     Arguments:
     - prev: tensor containing the output of the previous layer
     - n: number of nodes the new layer should contain
     - activation: activation function that should be used on the layer
     - lambtha: L2 regularization parameter
-    
+
     Returns:
     - The output of the new layer
     """
     # Define the L2 regularizer
     l2_regularizer = tf.contrib.layers.l2_regularizer(lambtha)
-    
-    # Create the layer using Dense, specifying the L2 regularizer for the kernel
+
+    # Create the layer using Dense, specifying
+    # the L2 regularizer for the kernel
     layer = tf.layers.Dense(units=n,
                             activation=activation,
                             kernel_initializer=tf.contrib.layers.variance_scaling_initializer(mode="FAN_AVG"),
                             kernel_regularizer=l2_regularizer)
-    
+
     # Return the output of the new layer applied
     # to the previous layer's output
     return layer(prev)
