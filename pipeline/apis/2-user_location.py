bfa17958168362e58fded102eda4fe8648b587da
elvisguybakunzi <e.bakunzi@alustudent.com>
2024-10-26 17:36:13 +0200

    location of GitHub user

--- pipeline/apis/2-user_location.py
+++ pipeline/apis/2-user_location.py
@@ -5,32 +5,17 @@ import requests
 import time
 
 
-def get_user_location(user_url):
-    """Fetch and print the location of a GitHub user or handle errors."""
-    try:
-        # Make the request to the provided user URL
-        response = requests.get(user_url)
+if __name__ == '__main__':
 
-        # Handle rate limiting (403 status code)
-        if response.status_code == 403:
-            reset_time = int(response.headers.get('X-RateLimit-Reset', 0))
-            # Calculate minutes until rate limit resets
-            reset_in_minutes = (reset_time - int(time.time())) // 60
-            print("Reset in {} min".format(reset_in_minutes))
-            return
-
-        # Handle user not found (404 status code)
-        if response.status_code == 404:
-            print("Not found")
-            return
-
-        # Handle successful request (200 status code)
-        if response.status_code == 200:
-            user_data = response.json()
-            location = user_data.get('location', 'Location not provided')
-            print(location)
-        else:
-            print("Unexpected error occurred")
-
-    except requests.RequestException as e:
-        print("Request failed: {}".format(e))
+    url = sys.argv[1]
+    headers = {'Accept': 'application/vnd.github.v3+json'}
+    response = requests.get(url, headers=headers)
+    if response.status_code == 200:
+        print(response.json()['location'])
+    if response.status_code == 404:
+        print('Not found')
+    if response.status_code == 403:
+        limit = int(response.headers['X-Ratelimit-Reset'])
+        start = int(time.time())
+        elapsed = int((limit - start) / 60)
+        print('Reset in {} min'.format(int(elapsed)))
